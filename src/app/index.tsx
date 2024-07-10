2f25b4b8ca92cb4052804a0492488c4261d49583
Jorge Cruz <j.alexcruz@gmail.com>
2024-07-10 17:31:05 -0300

    Install json-server

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { Text, View, ScrollView } from "react-native";
 //components
 import { Header } from "../components/header";
 import { Banner } from "../components/banner";
+import { Section } from "../components/section";
 
 // ajust statusbar
 import  Constants  from "expo-constants";
@@ -11,12 +12,20 @@ const statusBarsHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <ScrollView style={{flex:1}} className="bg-slate-200" showsVerticalScrollIndicator={false}>
+    <ScrollView style={{flex:1}} className="bg-white" showsVerticalScrollIndicator={false}>
       <View className="w-full px-4" style={{marginTop:statusBarsHeight + 8}}>
         <Header/>
         <Banner/>
         <Search/>
-      </View>
+
+        <Section 
+          name="Comidas em alta" 
+          label="Veja Mais" 
+          size="text-xl"
+          action={() => alert('Clicou')}
+        />
+        
+        </View>
     </ScrollView>
   );
 }
