38553d0415e7075cf393263ec77a19f44fdc9452
Jorge Cruz <j.alexcruz@gmail.com>
2024-07-09 21:06:19 -0300

    Deleted some files and folder

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:'#0ff'
+      }}
+    >
+      <Text>Vamos começar</Text>
+    </View>
+  );
+}
